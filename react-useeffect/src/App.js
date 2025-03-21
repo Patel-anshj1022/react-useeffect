df86d2c7edf5bbfaa1391efda7f6309ab5ffc2a2
Ansh Patel <patelanshj950.com>
2025-03-21 17:01:49 +0300

    updated the app.js with pokemon url

--- react-useeffect/src/App.js
+++ react-useeffect/src/App.js
@@ -1,14 +1,41 @@
 import './App.css';
-import { useEffect } from 'react';
-import Pokemon from './pages/pokemon.js';
+import React from'react';
+import { useEffect , useState} from "react";
+const Pokemon = () => {
+    const [data, setData] = useState([])
+    const[url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/?offset=${os}&limit=20")
 
-function App() {
-  useEffect(() => {}, []);
-  return (
-    <div>
-      <Pokemon/>
-    </div>
-  );
+    const fetchPokemon = () => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setData(data.results))
+        .catch(error => console.log("error fetching", error))
+    }
+    const handleNext = () =>{}
+    const handlePrevious = () =>{}
+
+
+
+
+
+    useEffect (() => {
+        fetchPokemon()
+    }, [] )
+    return(
+        <div>
+             <h2>Pokemon Characters</h2>
+            <ol>
+                {data.map((pokemon, index) => (
+                    <li
+                        key={index}>{pokemon.name}  {/* this will display the names */}
+                        {/*URL = {pokemon.url} {/* this will display the URL */}                 
+                </li>
+                ))}
+            </ol>
+            <button onClick={handlePrevious}>Previous</button>
+            <button onClick={handleNext}>Next</button>
+        </div>
+    );
 }
 
-export default App;
+export default Pokemon;
